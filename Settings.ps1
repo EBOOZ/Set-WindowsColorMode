b35634086f983a5ff344d761d2b84a833c89766a
Danny de Vries <danny@ebooz.nl>
2024-12-22 23:06:21 +0100

    Initial version

---
+++ Settings.ps1
@@ -0,0 +1,6 @@
+# Configure API key and City
+$ApiKey = "<API KEY>"
+$City = "<CITY>"
+# Configure wallpaper locations. Leave blank if you don't want to change the wallpaper
+$LightWallpaper = ""
+$DarkWallpaper = ""
